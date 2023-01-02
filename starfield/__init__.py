f32db5a50a201ad5f2b7e1b8d69307148c97cc8d
IsaacBreen <mail@isaacbreen.com>
2023-01-02 14:37:18 +0800

    add overload for repr option; various fixes

--- starfield/__init__.py
+++ starfield/__init__.py
@@ -1,14 +1,12 @@
-from typing import List, TypeVar, Type
+from typing import List, TypeVar, Type, Callable, Optional, Generator, overload
 
-from attrs import Attribute, fields
-from delegatefn import delegate
+from attrs import Attribute
 
 T = TypeVar("T")
 
-STARFIELD_KEY = type("STARFIELD_KEY", (), {})()
 
-
-def starfield(target_class: Type[T]) -> Type[T]:
+@overload
+def starfield(target_class: Type[T], attributes: List[Attribute]) -> List[Attribute]:
     """
     Modify a class to accept a "star field" argument. A star field is a special type of argument
     that is passed as a tuple of variadic positional arguments (i.e., "*args").
@@ -16,36 +14,96 @@ def starfield(target_class: Type[T]) -> Type[T]:
     :param target_class: The class to modify.
     :return: The modified class.
     """
-    if not hasattr(target_class, "__attrs_attrs__"):
-        raise TypeError(f"Expected attrs class, but did not find __attrs_attrs__ attribute on {target_class}")
-    # Get the list of attributes defined on the class
-    attributes: List[Attribute] = fields(target_class)
-    # Find the attribute with metadata[STARFIELD_KEY]["is_starfield"] == True
-    variadic_attributes = [attribute for attribute in attributes if attribute.metadata.get(STARFIELD_KEY, {}).get("is_starfield")]
-    # Raise an error if there is not exactly one such attribute
-    if len(variadic_attributes) != 1:
-        raise ValueError(
-            f"Expected exactly one attribute with init='*', got {len(variadic_attributes)}: {variadic_attributes}"
-        )
-    variadic_attribute = attributes[0]
 
-    def __init__(self, *args, **kwargs):
-        """
-        Modify the original `__init__` method of the class to accept a "star field" argument.
-        """
-        # Raise an error if the star field is passed as a keyword argument and there are also variadic positional arguments
-        if variadic_attribute.name in kwargs and len(args) > 0:
+
+@overload
+def starfield(repr: bool = False) -> Callable[[Type[T], List[Attribute]], List[Attribute]]:
+    """
+    Modify a class to accept a "star field" argument. A star field is a special type of argument
+    that is passed as a tuple of variadic positional arguments (i.e., "*args").
+
+    :param repr: Whether to include the star field in the class's __repr__.
+    :return: A field transformer that modifies a class to accept a star field.
+    """
+
+
+def starfield(
+    maybe_target_class: Optional[Type[T]] = None, attributes: Optional[List[Attribute]] = None, *, repr: bool = False
+) -> List[Attribute] | Callable[[Type[T], List[Attribute]], List[Attribute]]:
+    def _starfield(target_class: Type[T], attributes: List[Attribute]) -> List[Attribute]:
+        # Find the attribute with init="*".
+        variadic_attributes = [attribute for attribute in attributes if attribute.init == "*"]
+        # Raise an error if there is not exactly one such attribute
+        if len(variadic_attributes) != 1:
             raise ValueError(
-                f"Cannot pass star field {variadic_attribute.name} as a keyword argument when there are variadic positional arguments"
+                f"Expected exactly one attribute with init='*', got {len(variadic_attributes)}: {variadic_attributes}"
             )
-        # Store the tuple of variadic positional arguments as the value for the star field in the `kwargs` dictionary
-        kwargs[variadic_attribute.name] = args
-        # Call the original `__attrs_init__` method of the class, passing it the modified `kwargs` dictionary
-        self.__attrs_init__(**kwargs)
+        variadic_attribute = attributes[0]
 
-    # Modify the class to use the new `__init__` method
-    target_class.__init__ = __init__ # type: ignore[misc]
+        def __init__(self, *args, **kwargs):
+            """
+            Modify the original `__init__` method of the class to accept a "star field" argument.
+            """
+            # Raise an error if the star field is passed as a keyword argument and there are also variadic positional arguments
+            if variadic_attribute.name in kwargs and len(args) > 0:
+                raise ValueError(
+                    f"Cannot pass star field {variadic_attribute.name} as a keyword argument when there are variadic positional arguments"
+                )
+            # Store the tuple of variadic positional arguments as the value for the star field in the `kwargs` dictionary
+            kwargs[variadic_attribute.name] = args
+            # Call the original `__attrs_init__` method of the class, passing it the modified `kwargs` dictionary
+            self.__attrs_init__(**kwargs)
 
-    # Return a list of modified Attribute objects
-    return [attribute.evolve(kw_only=True) if attribute != variadic_attribute else attribute for attribute in
-        attributes]
+        # Modify the class to use the new `__init__` method
+        target_class.__init__ = __init__  # type: ignore[misc]
+
+        # Add a `__rich_repr__` method to the class
+        def __rich_repr__(self) -> Generator[str, None, None]:
+            """
+            Generate a rich representation of the class instance.
+
+            :return: A generator of strings that can be joined to form a rich representation of the class instance.
+            """
+            # Yield from the star field
+            yield from getattr(self, variadic_attribute.name)
+            # Yield the remaining attributes
+            for attribute in attributes:
+                if attribute is not variadic_attribute:
+                    yield attribute.name, getattr(self, attribute.name), attribute.default
+
+        # Set the `__rich_repr__` method on the class
+        target_class.__rich_repr__ = __rich_repr__
+
+        # If the `repr` argument is True, add a `__repr__` method to the class
+        if repr:
+            def __repr__(self) -> str:
+                """
+                Generate a rich representation of the class instance.
+
+                :return: A string that can be used as a rich representation of the class instance.
+                """
+                elements = []
+                for yielded in self.__rich_repr__():
+                    match yielded:
+                        case (name, value, default):
+                            if value != default:
+                                elements.append(f"{name}={value!r}")
+                        case name, value:
+                            elements.append(f"{name}={value!r}")
+                        case value:
+                            elements.append(f"{value!r}")
+                return f"{self.__class__.__name__}({', '.join(elements)})"
+
+            # Set the `__repr__` method on the class
+            target_class.__repr__ = __repr__
+
+        # Return a list of modified Attribute objects
+        return [attribute.evolve(kw_only=True) if attribute != variadic_attribute else attribute for attribute in
+            attributes]
+
+    if maybe_target_class is None and attributes is None:
+        return _starfield
+    elif maybe_target_class is not None and attributes is not None:
+        return _starfield(maybe_target_class, attributes)
+    else:
+        raise ValueError("Expected either no arguments or two arguments")
--- starfield/__init__.pyi
+++ starfield/__init__.pyi
@@ -1,4 +1,15 @@
-from attrs import Attribute
-from typing import List
+from typing import List, TypeVar
 
-def starfield(target_class: type, attributes: List[Attribute]) -> List[Attribute]: ...
+from attrs import Attribute
+
+T = TypeVar("T")
+
+
+@overload
+def starfield(target_class: Type[T], attributes: List[Attribute]) -> List[Attribute]:
+    ...
+
+
+@overload
+def starfield(*, repr: bool = False) -> Callable[[Type[T], List[Attribute]], List[Attribute]]:
+    ...
